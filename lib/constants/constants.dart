669676171cbbeae2565b0671e78a4432737cfc6d
buabaj <buabajerry@gmail.com>
2020-05-03 00:13:11 +0000

    wrote constants

---
+++ lib/constants/constants.dart
@@ -0,0 +1,15 @@
+class Constants {
+  static const String POPPINS = "Poppins";
+  static const String OPEN_SANS = "OpenSans";
+  static const String SKIP = "Skip";
+  static const String NEXT = "Next";
+  static const String SLIDER_HEADING_1 = "Welcome!";
+  static const String SLIDER_HEADING_2 = "Place Orders";
+  static const String SLIDER_HEADING_3 = "Secure Payments";
+  static const String SLIDER_DESC_1 =
+      "Buy or sell all your fashion products on GHstyles.";
+  static const String SLIDER_DESC_2 =
+      "Place Orders on any product and receive notifications on order status.";
+  static const String SLIDER_DESC_3 =
+      "Make payments through our highly secured gateways and receive instant verifications";
+}
