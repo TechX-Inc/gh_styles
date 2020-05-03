5cb5c6d361dcf050d031522924a1a0de8f4d563a
buabaj <buabajerry@gmail.com>
2020-05-03 00:44:54 +0000

    fixed one bug with landing_page.dart

--- lib/screens/landing_page.dart
+++ lib/screens/landing_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:gh_styles/constants/constants.dart';
 import 'package:gh_styles/ui_view/slider_layout_view.dart';
 import 'package:gh_styles/widgets/custom_font.dart';
 
