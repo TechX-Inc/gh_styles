4538b6e7da511c5b02c7e16930089cea54835254
buabaj <buabajerry@gmail.com>
2020-05-07 02:03:42 +0000

    wrote some code for splash screen but it doesn't work so i'll check it out later

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,55 @@
 import 'package:flutter/material.dart';
 import 'package:gh_styles/services/route_service.dart';
 import 'package:flutter/services.dart';
+import 'package:gh_styles/screens/landing_page.dart';
 import 'dart:io';
+import 'dart:async';
 
 void main() {
   runApp(MyApp());
+  runApp(new MaterialApp(
+    home: new SplashScreen(),
+    routes: <String, WidgetBuilder>{
+      '/landing_page': (BuildContext context) => new LandingPage()
+    },
+  ));
 }
 
-
-
-class MyApp extends StatelessWidget{
+class SplashScreen extends StatefulWidget {
   @override
+  _SplashScreenState createState() => new _SplashScreenState();
+}
 
+class _SplashScreenState extends State<SplashScreen> {
+  startTime() async {
+    var _duration = new Duration(seconds: 2);
+    return new Timer(_duration, navigationPage);
+  }
+
+  void navigationPage() {
+    Navigator.of(context).pushReplacementNamed('/landing_page');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    startTime();
+  }
+
+  @override
   Widget build(BuildContext context) {
- SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    return new Scaffold(
+      body: new Center(
+        child: new Image.asset('/assets/images/gh_style.png'),
+      ),
+    );
+  }
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
       statusBarColor: Colors.transparent,
       statusBarIconBrightness: Brightness.dark,
       statusBarBrightness:
@@ -25,11 +61,11 @@ class MyApp extends StatelessWidget{
 
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-       theme: ThemeData(
+      theme: ThemeData(
         primarySwatch: Colors.blue,
         platform: TargetPlatform.android,
       ),
-      initialRoute : '/',
+      initialRoute: '/',
       onGenerateRoute: RouteGenerator.generateRoute,
     );
   }
